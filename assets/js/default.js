9b4ff57e28186d76965c96e5ef21ddbec3e6cc3b
SeungSikHong <seungsik330@naver.com>
2023-01-04 23:42:20 +0900

    ✨ Add : add functions related to cookie add functions related to cookie

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,14 +1,16 @@
 const slideMove = () => {
 
 }
+
 const typing = (id, txt) => {
     let idx = 0;
-    let typingDiv = document.getElementById(id);
+    checkCookie('anm') === true ? '' : setCookie('anm','created',3);
+    const typingDiv = document.getElementById(id);
     const rmInterval = setInterval(() => {
         typingDiv.innerHTML = typingDiv.innerHTML + txt[idx];
         idx++;
         if (typingDiv.innerHTML.length === txt.length) {
-            let rmTimeout = setTimeout( () => {
+            const rmTimeout = setTimeout( () => {
                 document.getElementsByClassName('Romance')[0].style.display='none';
                 document.getElementsByClassName('asidel')[0].style.display='block';
                 document.getElementsByClassName('asider')[0].style.display='block';
@@ -17,4 +19,22 @@ const typing = (id, txt) => {
             clearInterval(rmInterval);
         }
     }, 180);
+}
+
+const setCookie = (key, value, expiredays) => {
+    let exdate = new Date();
+    exdate.setDate(exdate.getDate() + expiredays);
+    let cookie_value = escape(value) + ((expiredays == null) ? '' : '; expires=' + exdate.toUTCString());
+    document.cookie = encodeURIComponent(key) + '=' + encodeURIComponent(value) + ';expires=' + exdate.toUTCString() + ';path=/';
+}
+
+const getCookie = (key) => {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + key.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+const checkCookie = (key) => {
+    return getCookie(key) !== undefined ? true : false;
 }
